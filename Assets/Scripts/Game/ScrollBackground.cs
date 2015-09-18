65052b56fede41c60fb42bebfa790ed006a6387f
Justin Rempel <justin@tinymobgames.com>
2015-09-18 14:50:03 -0700

    Added a scrolling background and moved some scripts into the appropriate directories.

---
+++ Assets/Scripts/Game/ScrollBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollBackground : MonoBehaviour {
+
+	public float speed;
+
+	public float wrapAmount;
+
+	void Update () 
+	{
+		transform.Translate (Vector3.left * speed * Time.deltaTime);
+
+		if(transform.position.x <= -wrapAmount)
+		{
+			var pos = transform.localPosition;
+			pos.x += wrapAmount*2;
+			transform.localPosition = pos;
+		}
+	}
+}
