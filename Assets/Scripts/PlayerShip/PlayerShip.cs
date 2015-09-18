d5c62ad2d28b08d5cda7edce6e8c1bd38f149ea7
Justin Rempel <justin@tinymobgames.com>
2015-09-18 10:17:47 -0700

    Player ship now flies in from the left side of the screen.

--- Assets/Scripts/PlayerShip/PlayerShip.cs
+++ Assets/Scripts/PlayerShip/PlayerShip.cs
@@ -6,6 +6,7 @@ public class PlayerShip : MonoBehaviour {
 	float speed;
 	int lives;
 	int hp;
+	bool controlLocked;
 
 	// Use this for initialization
 	void Start () 
@@ -13,41 +14,76 @@ public class PlayerShip : MonoBehaviour {
 		speed = Tweakables.Instance.player.speed;
 		lives = Tweakables.Instance.player.lives;
 		hp = Tweakables.Instance.player.hp;
+
+		StartCoroutine (ForceMovement(Vector3.right, 5));
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		PollInput ();
-		ConstrainPosition ();
+		if(!controlLocked)
+		{
+			PollInput ();
+			ConstrainPosition ();
+		}
+
+	}
+
+	/// <summary>
+	/// Locks player input and moves in the specified direction.
+	/// </summary>
+	/// <param name="direction">Direction to move.</param>
+	/// <param name="distance">Distance to move in units.</param>
+	IEnumerator ForceMovement(Vector3 direction, float distance)
+	{
+		controlLocked = true;
+
+		float distanceTraveled = 0;
+
+		// move forward until destination is reached
+		while (distanceTraveled < distance)
+		{
+			Vector3 oldPosition = transform.position;
+			Move (direction);
+			distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+
+			yield return null; // wait until next frame
+		}
+
+		controlLocked = false;
 	}
 
 	void PollInput() 
 	{
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-			transform.Translate(Vector3.up * speed * Time.deltaTime);
+			Move(Vector3.up);
 		}
 
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
-			transform.Translate(Vector3.down * speed * Time.deltaTime);
+			Move(Vector3.down);
 		}
 
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.Translate(Vector3.left * speed * Time.deltaTime);
+			Move(Vector3.left);
 		}
 
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.Translate(Vector3.right * speed * Time.deltaTime);
+			Move(Vector3.right);
 		}
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			Fire ();
 		}
+	}
+
+	void Move(Vector3 direction)
+	{
+		transform.Translate(direction * speed * Time.deltaTime);
 	}
 
 	void ConstrainPosition() 
