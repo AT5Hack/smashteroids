b2aee0fcf2e0fbab43af4b1687cbd1be29146809
Justin Rempel <justin@tinymobgames.com>
2015-09-18 13:38:49 -0700

    Destroy objects that leave the play area

---
+++ Assets/DestroyOnExit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnExit : MonoBehaviour {
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		GameObject.Destroy (col.gameObject);
+	}
+}
