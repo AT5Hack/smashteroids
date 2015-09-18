a7e1c8b8a082206fdd6e8a26f10f3a3f95d00175
Max Loy <morac@morac.net>
2015-09-18 16:12:19 -0700

    Missed a change to inputmanager

--- Assets/Scripts/PlayerShip/PlayerShip.cs
+++ Assets/Scripts/PlayerShip/PlayerShip.cs
@@ -71,7 +71,7 @@ public class PlayerShip : MonoBehaviour {
 	{
 		Move(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0));
 
-		if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
+		if (Input.GetButton("Fire1"))
 		{
 			Fire ();
 		}
