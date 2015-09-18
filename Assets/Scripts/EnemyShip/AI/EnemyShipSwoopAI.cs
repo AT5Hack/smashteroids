5f9d9d4c6b91256481b24643472d92429e5ab1c8
Graham Davis <graham@tinymobgames.com>
2015-09-18 14:49:07 -0700

    updating swoop movement ai

--- Assets/Scripts/EnemyShip/AI/EnemyShipSwoopAI.cs
+++ Assets/Scripts/EnemyShip/AI/EnemyShipSwoopAI.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class EnemyShipSwoopAI : EnemyShipBaseAI {
 
+	public float swoopFreq = 2f;
+	public float swoopMagnitude = 3f;
+
+	private Vector3 movePos;
+
+	public override void Start () {
+		base.Start ();
+	
+		movePos = transform.position;
+	}
 
 	#region functions to be implemented/overriden
 	// do any thinking and calculations about how to act this step
@@ -12,7 +22,8 @@ public class EnemyShipSwoopAI : EnemyShipBaseAI {
 	
 	// do any movement after thinking this step
 	protected override void Move() {
-
+		movePos += Vector3.left * Time.deltaTime * ship.baseStats.speed;
+		transform.position = movePos + Vector3.up * Mathf.Sin (Time.time * swoopFreq) * swoopMagnitude;
 	}
 	
 	// do any shooting after thinking this step
