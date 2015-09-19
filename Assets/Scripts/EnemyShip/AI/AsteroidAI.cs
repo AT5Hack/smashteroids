d65ba7b89650ec4a22c7f54d4824daf5bfc8138f
Greg Patrick <greg@tinymobgames.com>
2015-09-18 17:14:19 -0700

    Example of having an AI do more than one thing... have Asteroids occasionally come in from the other side.

--- Assets/Scripts/EnemyShip/AI/AsteroidAI.cs
+++ Assets/Scripts/EnemyShip/AI/AsteroidAI.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class AsteroidAI : EnemyShipBaseAI {
 
+	private bool reverse = false;
+	
+	public override void Start () {
+		base.Start ();
+		
+		// Some asteroids will move in the opposite direction, coming in from behind the player
+		reverse = Random.value < 0.1;
+		if (reverse) transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+    }
+    
 	#region functions to be implemented/overriden
 	// should we think this step
 	protected override bool ShouldThink() {
@@ -22,7 +32,12 @@ public class AsteroidAI : EnemyShipBaseAI {
 	// do any movement after thinking this step
 	protected override void Move() {
 		// simply move to the left at a constant speed
-		transform.Translate(Vector3.left * ship.baseStats.speed * Time.deltaTime);
+		if (reverse) {
+			transform.Translate(Vector3.right * ship.baseStats.speed * 0.5f * Time.deltaTime);
+        }
+		else {
+			transform.Translate(Vector3.left * ship.baseStats.speed * Time.deltaTime);
+		}
 	}
 	
 	// do any shooting after thinking this step
